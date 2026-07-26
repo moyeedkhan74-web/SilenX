4fe975249c0531d03332f99dfd27b64fcca34694
Moyeed Khan <moyeedkhan74@gmail.com>
2026-07-26 19:59:42 +0530

    capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.slienx.app',
+  appName: 'SlienX',
+  webDir: 'dist'
+};
+
+export default config;
